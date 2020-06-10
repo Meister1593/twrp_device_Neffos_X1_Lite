34dcc193ef9d77ac68fdb37da46d4f71f0b20d2d
PLYSHKA <leruop@gmail.com>
2020-06-10 15:38:48 +0400

    Initial commit

---
+++ omni_X1_Lite.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2017 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/Neffos/X1_Lite
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, device/Neffos/X1_Lite/device.mk)
+
+
+PRODUCT_DEVICE := X1_Lite
+PRODUCT_NAME := omni_X1_Lite
+PRODUCT_BRAND := Neffos
+PRODUCT_MODEL := X1_Lite
+PRODUCT_MANUFACTURER := Neffos
+
